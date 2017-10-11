966abbf9de7b4bb68cd5744cdabe85b60d6395cc
netzilla <alpha.netzilla@gmail.com>
2017-10-11 15:05:03 +0900

    added a test code

--- spec/watson/conversation_spec.rb
+++ spec/watson/conversation_spec.rb
@@ -13,8 +13,9 @@ end
 shared_examples_for "conversation" do |storage|
 	manage = Watson::Conversation::ManageDialog.new(
 		username: [username],
+		password: [password],
 		workspace_id: [workspace_id],
-		storage: storage
+		storage: 'hash'
 	)
 
 
